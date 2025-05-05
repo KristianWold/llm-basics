631c2c641975aeeab4a002169f2307656809eb59
KristianWold <kristian.wold@hotmail.com>
2025-05-05 11:50:24 +0200

    Start implementing BPE tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -4,10 +4,10 @@ class TokenizerChar:
     def __init__(self, text):
         self.vocab = sorted(list(set(text)))
         self.vocab_size = len(self.vocab)
-        char_to_idx = {ch: i for i, ch in enumerate(self.vocab)}
+        self.token_to_idx = {ch: i for i, ch in enumerate(self.vocab)}
 
-        vocab = list(char_to_idx.keys())
-        indicies = list(char_to_idx.values())
+        vocab = list(self.token_to_idx.keys())
+        indicies = list(self.token_to_idx.values())
 
         default_value = -1
 
@@ -30,18 +30,3 @@ class TokenizerChar:
         text = self.table_detokenize.lookup(indices)
         text = tf.strings.reduce_join(text, axis=-1, separator="")
         return text
-    
-
-class TokenizerBPE:
-    def __init__(self, text):
-        self.tokenizer = tf.keras.layers.TextVectorization(standardize=None, split=None, ragged=False)
-        self.tokenizer.adapt([text])
-        self.vocab_size = self.tokenizer.vocabulary_size()
-        self.vocab = self.tokenizer.get_vocabulary()
-        self.vocab_dict = {word: i for i, word in enumerate(self.vocab)}
-
-    def tokenize(self, text):
-        return self.tokenizer(text)
-
-    def detokenize(self, indices):
-        return self.tokenizer.get_vocabulary()[indices]
