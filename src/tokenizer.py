10f29d0fe844a39bbeb84896c083d50f0c2e619e
KristianWold <kristian.wold@hotmail.com>
2025-05-05 12:21:16 +0200

    Finish BPE tokenizer and detokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,8 +1,28 @@
 import tensorflow as tf
+import unicodedata
+
+def normalize_to_ascii(s: str) -> str:
+    # 1) Decompose Unicode characters (e.g. é → e +  ́)
+    # 2) Drop the non-ASCII combining marks in the encode step
+    normalized = unicodedata.normalize('NFKD', s)
+    ascii_bytes = normalized.encode('ascii', 'ignore')
+    return ascii_bytes.decode('ascii')
+
+def word_split(line):
+    
+    normalized_line = normalize_to_ascii(line)
+    # Split into words
+    word_list = normalized_line.strip().split()
+    word_list = [list(word) for word in word_list]
+    return word_list
 
 class TokenizerChar:
-    def __init__(self, text):
-        self.vocab = sorted(list(set(text)))
+    def __init__(self, corpus):
+
+        # Flatten the list of lists into a single list
+        corpus_flatten = [item for sublist in corpus for item in sublist]
+    
+        self.vocab = sorted(list(set(corpus_flatten)))
         self.vocab_size = len(self.vocab)
         self.token_to_idx = {ch: i for i, ch in enumerate(self.vocab)}
 
